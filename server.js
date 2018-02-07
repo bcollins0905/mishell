66dfb31ae986357a3777e5315f72624f5d1891c9
bcollins0905 <b.w.collinssf@gmail.com>
2018-02-07 02:41:34 -0800

    created environment variables

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ var path = require('path');
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+  connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mishell_db"
+  });
+}
+// Make connection. 
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -35,3 +58,5 @@ app.listen(PORT, function() {
   });
 })
 
+// Export connection for our ORM to use.
+module.exports = connection;

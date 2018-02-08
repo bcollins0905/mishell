1800a6e9b58ac34970a3ce57c2b35e016f4554b8
bcollins0905 <b.w.collinssf@gmail.com>
2018-02-07 18:30:15 -0800

    change db initialization

--- server.js
+++ server.js
@@ -19,26 +19,26 @@ console.log('process.env: ', JSON.stringify(process.env, null, 2))
 console.log('')
 console.log('')
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-  console.log("Jaws : " + process.env.JAWSDB_URL)
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+//   console.log("Jaws : " + process.env.JAWSDB_URL)
+// } else {
 
-  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mishell_db"
-  });
-}
-// Make connection. 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "mishell_db"
+//   });
+// }
+// // Make connection. 
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 
 // Sets up the Express app to handle data parsing
